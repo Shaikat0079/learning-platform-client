6a705db8fefc786dcd998c631c48c2d823feaf34
Shaikat0079 <baruashaikat00790@gmail.com>
2022-10-25 15:55:17 +0600

    Firebase Added with out ENV

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA6yZjoupVNg1N9dvsz5b53SYtJEF0yXJg",
+    authDomain: "education-with-coding-language.firebaseapp.com",
+    projectId: "education-with-coding-language",
+    storageBucket: "education-with-coding-language.appspot.com",
+    messagingSenderId: "69975293222",
+    appId: "1:69975293222:web:8882764d703d9edbb395e7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
