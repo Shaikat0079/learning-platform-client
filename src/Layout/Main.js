21dac9e98f395934f078b9c4353dd3ebe4698f43
Shaikat0079 <baruashaikat00790@gmail.com>
2022-10-25 16:51:47 +0600

    Routes Setup

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Shared/Footer/Footer';
+import Header from '../Pages/Shared/Header/Header';
+import LeftSideNav from '../Pages/Shared/LeftSideNav/LeftSideNav';
 
 const Main = () => {
     return (
         <div>
-
+            <Header></Header>
             <Container>
                 <Row>
                     <Col lg='2' sm='6'>
-                        <h2>LeftNav</h2>
+                        <LeftSideNav></LeftSideNav>
                     </Col>
                     <Col lg='10' sm='6'>
                         <Outlet></Outlet>
                     </Col>
                 </Row>
             </Container>
+            <Footer></Footer>
         </div>
     );
 };
