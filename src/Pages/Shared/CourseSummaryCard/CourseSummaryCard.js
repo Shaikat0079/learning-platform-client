a07ee5492306680451a28c1cc8f6c42a5b775170
Shaikat0079 <baruashaikat00790@gmail.com>
2022-10-26 01:14:29 +0600

    Nav Bar and React Icons added

--- src/Pages/Shared/CourseSummaryCard/CourseSummaryCard.js
+++ src/Pages/Shared/CourseSummaryCard/CourseSummaryCard.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import { MdOutlineStar } from "react-icons/md";
+import { MdMonetizationOn } from "react-icons/md";
 
 const CourseSummaryCard = ({ courses }) => {
-    const { name, id, description } = courses;
+    const { name, id, description, image_url, rating } = courses;
     return (
         <div>
             <Card className="text-center">
-                <Card.Header>Featured</Card.Header>
+                <Card.Header>WELCOME to {name}</Card.Header>
                 <Card.Body>
-                    <Card.Title>{name}</Card.Title>
+                    <Card.Img style={{ height: '250px' }} variant="top" src={image_url} />
                     <Card.Text>
                         {description.length > 200 ?
                             <>{description.slice(0, 200) + '...'} <Link to={`/course/${id}`}>Read More</Link></>
@@ -20,7 +22,10 @@ const CourseSummaryCard = ({ courses }) => {
                     </Card.Text>
                     {/* <Button variant="dark"><Link className='text-white' to={`/course/${id}`}>Go to</Link></Button> */}
                 </Card.Body>
-                <Card.Footer className="text-muted">2 days ago</Card.Footer>
+                <Card.Footer className="d-flex justify-content-between">
+                    <div><MdOutlineStar /><span>{rating}</span></div>
+                    <div>< MdMonetizationOn /><span>{rating}</span></div>
+                </Card.Footer>
             </Card>
         </div>
     );
