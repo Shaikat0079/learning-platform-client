f295ebe29fed381ab636842a660279f2899e40f6
Shaikat0079 <baruashaikat00790@gmail.com>
2022-10-25 17:23:18 +0600

    Courses Name Loaded in LeftNav Bar

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5002/courses')
+            .then(res => res.json())
+            .then(data => setCourses(data));
+    }, [])
     return (
         <div>
-            <h2>Left Side Nav</h2>
+            <h4>Total Courses : {courses.length}</h4>
+            <div>
+                {
+                    courses.map(course => <p key={course.id}>
+                        <Link >{course.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
